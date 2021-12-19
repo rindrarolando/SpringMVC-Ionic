5bcb745043a043fb10fd3bd64cedc8f2fe5a964e
Rindra Rolando <rindrarolando7@gmail.com>
2021-12-19 10:51:41 +0300

    Creation du projet
    copie des fichiers après creation sur start.spring.io

---
+++ src/test/java/com/projet/cloudmobile/CloudMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.cloudmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
