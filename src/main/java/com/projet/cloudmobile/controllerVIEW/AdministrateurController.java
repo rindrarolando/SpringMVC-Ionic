fb82d632aed6fc1d7026d18f8e89a0d9fd602af6
Rindra Rolando <rindrarolando7@gmail.com>
2022-01-14 21:28:02 +0300

    Exemple View JSP
    Exemple fotsiny fa Michael miteny hoe tsy mandeha nenlery

--- src/main/java/com/projet/cloudmobile/controllerVIEW/AdministrateurController.java
+++ src/main/java/com/projet/cloudmobile/controllerVIEW/AdministrateurController.java
@@ -1,7 +1,12 @@
 package com.projet.cloudmobile.controllerVIEW;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdministrateurController {
+    @RequestMapping("/")
+    public String exemple(){
+        return "exemple";
+    }
 }
