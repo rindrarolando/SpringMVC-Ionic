55b8ccb961136a66e2cf792ad98837a18a4123a3
Rindra Rolando <rindrarolando7@gmail.com>
2022-01-30 17:03:36 +0300

    Token d'authentification admin

--- src/main/java/com/projet/cloudmobile/controllerAPI/SignalementRestController.java
+++ src/main/java/com/projet/cloudmobile/controllerAPI/SignalementRestController.java
@@ -17,10 +17,17 @@ import java.util.Objects;
 public class SignalementRestController {
     @CrossOrigin
     @GetMapping("/getSignalements")
-    public ResponseEntity<List<Signalement>> getSignalements(){
-
+    public ResponseEntity<List<Signalement>> getSignalements(@RequestHeader("token") String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<List<Signalement>>(new SignalementDao().getAllSignalement(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
@@ -40,11 +47,18 @@ public class SignalementRestController {
 
     @CrossOrigin
     @GetMapping("/deleteSignalement")
-    public ResponseEntity deleteSignalement(@RequestParam(value = "id")String id){
-        SignalementDao s = new SignalementDao();
-        s.remove(Long.valueOf(id));
-        return new ResponseEntity(HttpStatus.ACCEPTED);
-
+    public ResponseEntity deleteSignalement(@RequestHeader("token")String token,@RequestParam(value = "id")String id){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                new SignalementDao().remove(Long.valueOf(id));
+                return new ResponseEntity(HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
 
     }
 
@@ -65,180 +79,358 @@ public class SignalementRestController {
 
     @CrossOrigin
     @GetMapping("/statEtat1")
-    public ResponseEntity<Long> getStatE1(){
-
+    public ResponseEntity<Long> getStatE1(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat1(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statEtat2")
-    public ResponseEntity<Long> getStatE2(){
-
+    public ResponseEntity<Long> getStatE2(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat2(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat3")
-    public ResponseEntity<Long> getStatE3(){
-
+    public ResponseEntity<Long> getStatE3(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat3(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/statEtat1N")
-    public ResponseEntity<Long> getStatE1N(){
-
+    public ResponseEntity<Long> getStatE1N(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat1N(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat2N")
-    public ResponseEntity<Long> getStatE2N(){
-
+    public ResponseEntity<Long> getStatE2N(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat2N(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat3N")
-    public ResponseEntity<Long> getStatE3N(){
-
+    public ResponseEntity<Long> getStatE3N(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat3N(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/statEtat1E")
-    public ResponseEntity<Long> getStatE1E(){
-
+    public ResponseEntity<Long> getStatE1E(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat1E(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat2E")
-    public ResponseEntity<Long> getStatE2E(){
-
+    public ResponseEntity<Long> getStatE2E(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat2E(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat3E")
-    public ResponseEntity<Long> getStatE3E(){
-
+    public ResponseEntity<Long> getStatE3E(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat3E(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/statEtat1T")
-    public ResponseEntity<Long> getStatE1T(){
-
+    public ResponseEntity<Long> getStatE1T(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat1T(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat2T")
-    public ResponseEntity<Long> getStatE2T(){
-
+    public ResponseEntity<Long> getStatE2T(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat2T(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @CrossOrigin
     @GetMapping("/statEtat3T")
-    public ResponseEntity<Long> getStatE3T(){
-
+    public ResponseEntity<Long> getStatE3T(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<Long>(new SignalementDao().getStatEtat3T(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/SignalementByUtil")
-    public ResponseEntity<List<Signalement>> getSignalementByUtil(@RequestParam(value = "username")String username){
-
+    public ResponseEntity<List<Signalement>> getSignalementByUtil(@RequestHeader("token")String token,@RequestParam(value = "username")String username){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<List<Signalement>>(new SignalementDao().getSignalementByUtil(username), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/statistiqueEtat")
-    public ResponseEntity<List<Signalement>> getStatistiqueEtat(){
-
+    public ResponseEntity<List<Signalement>> getStatistiqueEtat(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
                 return new ResponseEntity<List<Signalement>>(new SignalementDao().getStatEtat(), HttpStatus.ACCEPTED);
-
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/signalementRegion")
-    public List<Signalement> getSignalementByRegion(@RequestParam(value="id")String id){
-        return new SignalementDao().getSignalementByRegion(Integer.parseInt(id));
+    public ResponseEntity<List<Signalement>> getSignalementByRegion(@RequestHeader("token")String token,@RequestParam(value="id")String id){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<List<Signalement>>(new SignalementDao().getSignalementByRegion(Integer.parseInt(id)), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @CrossOrigin
     @GetMapping("/statistique/region")
-    public ArrayList<HashMap<String, Object>> getStatReg(){
-
-        return new SignalementDao().getStatistiqueRegion();
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatReg(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueRegion(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
     }
     @CrossOrigin
     @GetMapping("/statistique/region/encours")
-    public ArrayList<HashMap<String, Object>> getStatReg1(){
-
-        return new SignalementDao().getStatistiqueRegion1();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatReg1(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueRegion1(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statistique/region/nouveau")
-    public ArrayList<HashMap<String, Object>> getStatReg2(){
-
-        return new SignalementDao().getStatistiqueRegion2();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatReg2(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueRegion2(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statistique/region/termine")
-    public ArrayList<HashMap<String, Object>> getStatReg3(){
-
-        return new SignalementDao().getStatistiqueRegion3();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatReg3(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueRegion3(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
     //STATISTIQUE TYPE
     @CrossOrigin
     @GetMapping("/statistique/type")
-    public ArrayList<HashMap<String, Object>> getStatType(){
-
-        return new SignalementDao().getStatistiqueType();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatType(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueType(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statistique/type/encours")
-    public ArrayList<HashMap<String, Object>> getStatType1(){
-
-        return new SignalementDao().getStatistiqueTypeCas1();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatType1(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueTypeCas1(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statistique/type/nouveau")
-    public ArrayList<HashMap<String, Object>> getStatType2(){
-
-        return new SignalementDao().getStatistiqueTypeCas2();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatType2(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueTypeCas2(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @CrossOrigin
     @GetMapping("/statistique/type/termine")
-    public ArrayList<HashMap<String, Object>> getStatType3(){
-
-        return new SignalementDao().getStatistiqueTypeCas3();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatType3(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueTypeCas3(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     //STATISTIQUE UTILISATEUR
     @CrossOrigin
     @GetMapping("/statistique/utilisateur")
-    public ArrayList<HashMap<String, Object>> getStatUtil(){
-
-        return new SignalementDao().getStatistiqueUtil();
-
+    public ResponseEntity<ArrayList<HashMap<String, Object>>> getStatUtil(@RequestHeader("token")String token){
+        TokenDao dao = new TokenDao();
+        try{
+            if(dao.isAdminToken(token)==true) {
+                return new ResponseEntity<ArrayList<HashMap<String, Object>>>(new SignalementDao().getStatistiqueUtil(), HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
